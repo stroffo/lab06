5a536d0d7293e03b5228af8356149b289425d85f
Nicola Stroffolino <nicola.stroffolino@studio.unibo.it>
2025-11-03 00:07:32 +0100

    Ex. 64 SocialNetworkUserImpl addFollowedUser, getFollowedUsersInGroup full implementation

--- 64-design-with-maps/src/main/java/it/unibo/collections/social/impl/SocialNetworkUserImpl.java
+++ 64-design-with-maps/src/main/java/it/unibo/collections/social/impl/SocialNetworkUserImpl.java
@@ -7,14 +7,11 @@ package it.unibo.collections.social.impl;
 import it.unibo.collections.social.api.SocialNetworkUser;
 import it.unibo.collections.social.api.User;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * This will be an implementation of
@@ -38,6 +35,7 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      *
      * think of what type of keys and values would best suit the requirements
      */
+    private final Map<String, LinkedList<U>> followedUsers = new HashMap<>();
 
     /*
      * [CONSTRUCTORS]
@@ -64,12 +62,15 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      *            application
      */
     public SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
-        super(null, null, null, 0);
+        super(name, surname, user, userAge);
     }
 
     /*
      * 2) Define a further constructor where the age defaults to -1
      */
+    public SocialNetworkUserImpl(final String name, final String surname, final String user) {
+        super(name, surname, user, -1);
+    }
 
     /*
      * [METHODS]
@@ -78,7 +79,9 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      */
     @Override
     public boolean addFollowedUser(final String circle, final U user) {
-        return false;
+        followedUsers.putIfAbsent(circle, new LinkedList<>());
+        followedUsers.get(circle).add(user);
+        return true;
     }
 
     /**
@@ -88,8 +91,9 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      */
     @Override
     public Collection<U> getFollowedUsersInGroup(final String groupName) {
-        return null;
-    }
+        var usersInGroup = followedUsers.get(groupName);
+        return usersInGroup == null ? new LinkedList<>() : new LinkedList<>(usersInGroup);
+    } 
 
     @Override
     public List<U> getFollowedUsers() {
